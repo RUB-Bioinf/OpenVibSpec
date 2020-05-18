2744d8ac6d64146255f05247a5edaaa4258e93b1
arnrau <arne.raulf@googlemail.com>
2020-05-18 13:17:04 +0200

    CI actions 2

--- setup.py
+++ setup.py
@@ -1,22 +1,26 @@
-#!/usr/bin/env python
-from setuptools import setup
+from distutils.core import setup
+
+from setuptools import find_packages
 
 
-if __name__ == '__main__':
+setup(
+	name="OpenVibSpec",
+	packages=find_packages(),
+	version="0.0.1",
+	licence="bsd-3-clause-clear",
+	description="",
+	author="Arne Peter Raulf",
+	url="https://github.com/arnrau/OpenVibSpec",
+	keywords=["spectroscopy","deep-learning", "machine-learning"],
+	classifiers=[
+		"Development Status :: 4 - Beta"
+		"Intended Audience :: Developers",
+		"Programming Language :: Python 3",
+		"Programming Language :: Python 3.6",
 
-    setup(
-        name="OpenVibSpec",
-        version="0.0.1",
-        package_dir={'': 'scr'},
-        packages=['openvibspec'],
-        include_package_data=True,
-        install_requires=[
-            'matplotlib',
-            'keras',
-            'numpy',
-            'scipy',
-            'scikit-learn',
-            'h5py',
-            'tensorflow',
-        ],
-    )
+
+	],
+
+
+
+	)
